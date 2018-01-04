75f34e95c2f61dceca21d0cb38d1f4d26c6df9bf
Alice <alicewc95@gmail.com>
2018-01-03 18:10:08 -0800

    checkpoint-10

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,3 +1,5 @@
-class Wiki < ApplicationRecord
+class Wiki < ActiveRecord::Base
   belongs_to :user
+  has_many :collaborators, dependent: :destroy
+  has_many :users, through: :collaborators
 end
--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,16 @@
 
 ActiveRecord::Schema.define(version: 20180103015118) do
 
+  create_table "collaborators", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "wiki_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "collaborators", ["user_id"], name: "index_collaborators_on_user_id"
+  add_index "collaborators", ["wiki_id"], name: "index_collaborators_on_wiki_id"
+
   create_table "models", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
