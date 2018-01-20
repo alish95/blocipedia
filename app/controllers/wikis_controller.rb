7653c41f549be85e0dade65170cb908b420827dd
Alice <alicewc95@gmail.com>
2018-01-20 15:30:35 -0800

    Update wikis_controller.rb

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -1,67 +1,60 @@
 class WikisController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     @wikis = Wiki.all
   end
 
   def show
     @wiki = Wiki.find(params[:id])
-    authorize @wiki
   end
 
   def new
     @wiki = Wiki.new
-    authorize @wiki
   end
 
   def create
     @wiki = Wiki.new
-    authorize @wiki
     @wiki.title = params[:wiki][:title]
     @wiki.body = params[:wiki][:body]
     @wiki.private = params[:wiki][:private]
-    @wiki.user_id = current_user.id
 
     if @wiki.save
       flash[:notice] = "Wiki was saved."
-      redirect_to @wiki
+      redirect_to wikis_path
     else
-      flash.now[:alert] = "There was an error saving the wiki. Try again."
+      flash.now[:alert] = "There was an error saving the wiki. Please try again."
       render :new
     end
   end
 
   def edit
     @wiki = Wiki.find(params[:id])
-    authorize @wiki
   end
 
   def update
-  	@wiki = Wiki.find(params[:id])
-  	authorize @wiki
-  	@wiki.title = params[:wiki][:title]
-  	@wiki.body = params[:wiki][:body]
-  	@wiki.private = params[:wiki][:private]
+    @wiki = Wiki.find(params[:id])
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
 
-  	if @wiki.save
-  		flash[:notice] = "Wiki was updated."
-  		redirect_to @wiki
-  	else
-  		flash.now[:alert] = "There was an error saving the wiki. Please try again."
-  		render :edit
-  	end
+    if @wiki.save
+      flash[:notice] = "Wiki was updated."
+      redirect_to wikis_path
+    else
+      flash.now[:alert] = "There was an error saving the wiki. Please try again."
+      render :edit
+    end
   end
 
   def destroy
+    @wiki = Wiki.find(params[:id])
 
-	  @wiki = Wiki.find(params[:id])
-	  authorize @wiki
-
-  	if @wiki.destroy
-  		flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
-  		redirect_to wikis_path
-  	else
-  		flash.now[:alert] = "There was an error deleting the post."
-  		render :show
-  	end
+    if @wiki.destroy
+      flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+      redirect_to wiki_path
+    else
+      flash.now[:alert] = "There was an error deleting the wiki."
+      render :show
+    end
   end
 end
